d3aa5d3ca03f7f73547c65af46160cf78dc1272a
tresor <tshims79@gmail.com>
2023-06-25 15:41:26 +0200

    Now we implement our divide method

---
+++ Uqs.Arithmetic/Division.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uqs.Arithmetic
+{
+    public class Division
+    {
+        public static decimal Divide(int dividend, int divisor)
+        {
+            decimal quotinet = dividend / divisor;
+            return quotinet;    
+        }
+    }
+}
